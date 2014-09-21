1866c649838ee647f15e75234d87e89662f4c3fc
rey <rey@live.jp>
2014-09-21 19:57:24 +0800

    Fixed Mesh List Table Widget

--- mednus_code/PMeshViewerCallback.h
+++ mednus_code/PMeshViewerCallback.h
@@ -1,16 +1,18 @@
 #ifndef PMESHVIEWERCALLBACK_H
 #define PMESHVIEWERCALLBACK_H
 #include <vtkCommand.h>
+#include <vtkObjectBase.h>
 #include "PMeshViewer.h"
 
 class PMeshViewerCallback: public vtkCommand
 {
 public:
     static PMeshViewerCallback *New() { return new PMeshViewerCallback(); }
-    //void Execute(vtkObject *caller, unsigned long eventId, void *callData);
-    void Execute( vtkObject *caller,
-        unsigned long eventId, void *callData);
+    void Execute(vtkObject *caller, unsigned long eventId, void *callData);
     PMeshViewer *viewer;
+    void PrintSelf(ostream& os, vtkIndent indent){};
+    void PrintHeader(ostream& os, vtkIndent indent){};
+    void PrintTrailer(ostream& os, vtkIndent indent){};
 
 protected:
     PMeshViewerCallback();
