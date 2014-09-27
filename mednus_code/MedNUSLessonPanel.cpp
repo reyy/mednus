5cb57f04b1aec135da62f5006fea97d86b9d98b8
JayChua <JayChua@localhost>
2014-09-27 17:07:32 +0800

    - Added MedNUSLessonPanel Widget. - Shifted layout for model properties. - Tested color scheme for MedNUS.

---
+++ mednus_code/MedNUSLessonPanel.cpp
@@ -0,0 +1,21 @@
+/* MedNUSLessonPanel.h
+
+   Table widget for lesson plan organization.
+
+   Author: Jay Chua
+*/
+
+#include "MedNUSLessonPanel.h"
+#include <QtGui>
+#include <QPalette>
+
+#include <iostream>
+
+MedNUSLessonPanel::MedNUSLessonPanel(): QTableWidget(0, 0)
+{
+}
+
+
+MedNUSLessonPanel::~MedNUSLessonPanel()
+{
+}
