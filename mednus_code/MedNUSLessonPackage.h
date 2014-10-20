ca4efd5df0ad47a65232a1a039901ce70a536d1c
JayChua <JayChua@localhost>
2014-10-21 00:57:34 +0800

    Added functionality to add, remove and clear type of lessons to use in MedNUS. Ability to customise different text, content and visibility.

--- mednus_code/MedNUSLessonPackage.h
+++ mednus_code/MedNUSLessonPackage.h
@@ -11,17 +11,32 @@ class MedNUSLessonPackage : public QWidget
 {
     Q_OBJECT
 private:
+    int _y;
+    int _tone;
+    int _height;
+    int _collapsedHeight;
+    QString _title;
     QLabel *_background;
     QLabel *_moduleTitle;
     QLabel *_subHeader;
     QLabel *_description;
+    bool _collapse;
 
 public:
     explicit MedNUSLessonPackage(QWidget *parent = 0);
     ~MedNUSLessonPackage();
-
-protected:
-    void mousePressEvent ( QMouseEvent * event ) ;
+    void setY(int value);
+    int getY();
+    void setTitle(QString value);
+    QString getTitle();
+    void setSubHeader(QString value);
+    void setDescription(QString value);
+    void setTone(int value);
+    int getHeight();
+    int getCollapseHeight();
+    void toggleCollapse();
+    void toggleCollapse(bool value);
+    void updateGUI();
 
 public slots:
 
