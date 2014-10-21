f5675debc2b382ba55a1a614072e7034bda5569a
JayChua <JayChua@localhost>
2014-10-21 16:31:21 +0800

    Added logo image and a smaller logo image.

--- mednus_code/MedNUSContentPanel.cpp
+++ mednus_code/MedNUSContentPanel.cpp
@@ -4,8 +4,8 @@ MedNUSContentPanel::MedNUSContentPanel(QWidget *parent) :
     QTabWidget(parent)
 {
     this->setMinimumWidth(800-300);
-    this->setMinimumHeight(600-64);
-    this->setMaximumHeight(600-64);
+    this->setMinimumHeight(600-32);
+    this->setMaximumHeight(600-32);
 
     this->setTabsClosable(true);
     this->setMovable(true);
--- mednus_code/MedNUSUserBar.cpp
+++ mednus_code/MedNUSUserBar.cpp
@@ -4,11 +4,11 @@ MedNUSUserBar::MedNUSUserBar(QWidget *parent) :
     QWidget(parent)
 {
     this->setMinimumWidth(300);
-    this->setMinimumHeight(64);
-    this->setMaximumHeight(64);
+    this->setMinimumHeight(32);
+    this->setMaximumHeight(32);
 
     _background = new QLabel(this);
-    _background->setGeometry(QRect(this->x(), this->y(), 300, 64));
+    _background->setGeometry(QRect(this->x(), this->y(), 300, 32));
     _background->setStyleSheet("background-color: #1c4f6e;");
 }
 
