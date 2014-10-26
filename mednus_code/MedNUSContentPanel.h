ec71a443cadee894d68f4236a02f241a4b94fa23
rey <rey@localhost>
2014-10-26 12:52:11 +0800

    Added support for Closing Tabs & Auto-Fill closed tab space

--- mednus_code/MedNUSContentPanel.h
+++ mednus_code/MedNUSContentPanel.h
@@ -4,6 +4,25 @@
 #include <QTabWidget>
 #include <QGridLayout>
 
+class MedNUSTab : public QTabWidget
+{
+    Q_OBJECT
+public:
+    explicit MedNUSTab(QWidget *parent = 0)
+    {connect(this,SIGNAL(tabCloseRequested(int)),this,SLOT(closeTab(int)));}
+    ~MedNUSTab(){}
+signals:
+    void noMoreTabs(MedNUSTab*);
+ public slots:
+    void closeTab(int index)
+    {
+        this->removeTab(index);
+        if(this->count()==0)
+            emit noMoreTabs(this);
+    }
+
+};
+
 class MedNUSContentPanel : public QWidget
 {
     Q_OBJECT
@@ -11,13 +30,15 @@ public:
     explicit MedNUSContentPanel(QWidget *parent = 0);
     ~MedNUSContentPanel();
 
-    QTabWidget tabList[4];
+    MedNUSTab tabList[4];
     QGridLayout *layout;
     int counter =2;
     void addTab(QWidget*,QString);
 signals:
 
 public slots:
-
+    void closeTab(MedNUSTab *index);
 };
+
+
 #endif // MEDNUSCONTENTPANEL_H
