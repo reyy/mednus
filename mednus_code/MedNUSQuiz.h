75dd687cac4347688020eb4f4ba0396ace0f12e9
melvynqwek <melvynqwek@localhost>
2014-10-31 02:03:41 +0800

    - Fixed bug where you can only choose one radio button. - Quiz can now be graded. - TODO: Need to de-hardcode the questions and answers. - TODO: Need to save quiz in separate file. (JSON)

--- mednus_code/MedNUSQuiz.h
+++ mednus_code/MedNUSQuiz.h
@@ -7,18 +7,25 @@
 #include <QGroupBox>
 #include <QVBoxLayout>
 #include <QRadioButton>
+#include <QButtonGroup>
+#include <QPushButton>
 #include <QVector>
 #include <QString>
 
+// For debugging
+#include<QTextStream>
+
+// MedNUSQuizQuestion is not used for now.
 class MedNUSQuizQuestion
 {
 public:
-    MedNUSQuizQuestion(QWidget *parent, QVBoxLayout *layout);
+    MedNUSQuizQuestion(QWidget *parent, QGroupBox *box);
     ~MedNUSQuizQuestion();
 
 private:
     QLabel _questionTextLabel;
     QRadioButton* _option1;
+    QButtonGroup* _optionButtonGroup;
 };
 
 class MedNUSQuiz : public QWidget
@@ -29,16 +36,23 @@ public:
     explicit MedNUSQuiz(QString filename, QWidget *parent = 0);
     ~MedNUSQuiz();
 
+
 protected:
-    QVector<MedNUSQuizQuestion*> *_questionList;
+    //QVector<MedNUSQuizQuestion*> *_questionList;
 
     QGroupBox* _content;
     QVBoxLayout* _layout;
 
+    QVector<QButtonGroup*> _questionOptionGroup;
+
+    QPushButton* _markButton;
+    QVector<int> _correctAnswerList;
+    int _score;
+
 signals:
 
 public slots:
-
+    void markQuiz();
 };
 
 #endif // MEDNUSQUIZ_H
