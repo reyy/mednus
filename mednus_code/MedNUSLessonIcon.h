f0c12a97c118ad6d4bf1c78c8a0aca66af131d66
melvynqwek <xtrmepower@gmail.com>
2015-03-12 14:46:53 +0800

    Added tester class for MedNUSQuiz

---
+++ mednus_code/MedNUSLessonIcon.h
@@ -0,0 +1,58 @@
+#ifndef MedNUSLessonIcon_H
+#define MedNUSLessonIcon_H
+
+#include <QLabel>
+#include <QMouseEvent>
+#include <QPushButton>
+
+#include "MedNUSAUISettings.h"
+
+class MedNUSLessonIcon : public QWidget
+{
+    Q_OBJECT
+public:
+     MedNUSLessonIcon(QString path, fileType filetype, QWidget *parent);
+    ~MedNUSLessonIcon();
+
+     void setMode(interfaceMode mode);
+     void updatePosition(float packageX, float packageY, float x, float y);
+     void setSelected(bool value);
+     void setVisible(bool value);
+     void setScrollBarSpace(bool value);
+
+protected:
+    void setHighlight(bool status);
+    void mousePressEvent(QMouseEvent* event);
+
+private:
+    bool _scrollBarExist;
+    bool _selected;
+    QLabel *_highlight;
+    QLabel *_icon;
+    QLabel *_text;
+    QString _path;
+    QString _filename;
+    QWidget *_parent;
+    fileType _filetype;
+
+    float _x;
+    float _y;
+
+    interfaceMode _currentMode;
+    QPushButton *_btDelete;
+    QPushButton *_btMisc;
+
+signals:
+    void emitOpenFile(QString, QString, int);
+
+public slots:
+    void tabOpened(QString);
+    void tabClosed(QString);
+
+protected slots:
+    void activateMISC();
+    void deleteSelection();
+};
+
+
+#endif // MedNUSLessonIcon_H
