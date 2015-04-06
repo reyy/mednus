1eeda6374274f80b922d8163c328ebcaccb26fa3
rey <rey@localhost>
2015-04-06 15:18:07 +0800

    [BugFix] MainWindow Crash on login sometimes + Network test cases

--- mednus_test/tst_mednusnetworktester.cpp
+++ mednus_test/tst_mednusnetworktester.cpp
@@ -28,6 +28,7 @@ void MedNUSNetworkTester::cleanupTestCase()
 
 void MedNUSNetworkTester::BluebellHTTPSTest()
 {
+    //HTTPS Server Connection Test
     QSignalSpy m_pSignalSpy(&(network->mgr), SIGNAL(finished(QNetworkReply*)));
     network->fileDownload();
     m_pSignalSpy.wait();
@@ -39,19 +40,39 @@ void MedNUSNetworkTester::BluebellHTTPSTest()
         for(int index2 = 0;index2 < listItem.size();index2++)
         {
             qDebug(listItem[index2].toString().toStdString().c_str());
+            //Check success
+            QVERIFY(listItem[index2].toBool() == true);
         }
     }
 }
 
 void MedNUSNetworkTester::loginTest()
 {
-
-    QSignalSpy spy(network, SIGNAL(loginResults(bool,QString,QString)));
-//    //QVERIFY2(false, "Failure");
+    //Failed Login Test
+    QSignalSpy spy(network, SIGNAL(loginReply(QJsonObject)));
     network->login("NUSSTU\a0111823","123", true);
+    spy.wait();
+    QList<QVariant> arguments = spy.takeFirst();
+    QVERIFY(arguments.at(0).type() == QJsonObject);
+    QVERIFY(dynamic_cast<QJsonObject>(arguments.at(0))
+            ["Success"].toBool() == false);
+}
+
+void MedNUSNetworkTester::downloadLessonListTest()
+{
+    //Failed Lesson Download Test
+    QJsonObject json;
+    json["UserID"] = "NUSSTU\a0111823";
+    json["Name"] = "MYNAME";
+
+    QSignalSpy spy(network, SIGNAL(loginResults(bool,QString,QString,interfaceMode)));
+    network->profileReply(json);
     spy.wait();
     QList<QVariant> arguments = spy.takeFirst();
     QVERIFY(arguments.at(0).type() == QVariant::Bool);
     QVERIFY(arguments.at(1).type() == QVariant::String);
     QVERIFY(arguments.at(2).type() == QVariant::String);
+    //Should fail
+    QVERIFY(arguments.at(0).toBool() == false);
+    QVERIFY(dynamic_cast<interfaceMode>(arguments.at(3)) == NONE);
 }
