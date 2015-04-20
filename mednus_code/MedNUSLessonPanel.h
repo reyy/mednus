cc5d0d6ba19d5adc59337c66ae45d6ab4217923e
rey <rey@localhost>
2015-04-20 18:07:52 +0800

    Linked Create New Lesson button (layering issue still there)

--- mednus_code/MedNUSLessonPanel.h
+++ mednus_code/MedNUSLessonPanel.h
@@ -60,6 +60,7 @@ private:
 
 public slots:
     void addLesson(QString title, QString subTitle, QString description, QStringList directory, QString storyFile);
+    void addLesson();
     void removeLesson(MedNUSLessonPackage*);
     void callOpenFile(QString str, QString title, int type);
 
